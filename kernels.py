3d90b10aacba9292713a0942eb9a41d29bffdd47
GabSan <santin.gabriele@gmail.com>
2019-11-24 20:29:51 +0100

    Initial commit

--- kernels.py
+++ kernels.py
@@ -22,17 +22,37 @@ class RBF(Kernel):
         self.rbf_type = rbf_type
         self.ep = ep
         if self.rbf_type == 'gauss':
-            self.rbf = lambda ep, r: np.exp(- (ep * r) ** 2)
+            self.rbf = lambda ep, r: np.exp(-(ep * r) ** 2)
         elif self.rbf_type == 'gauss_tanh':
-            self.rbf = lambda ep, r: np.exp(- (ep * np.tanh(r)) ** 2)
+            self.rbf = lambda ep, r: np.exp(-(ep * np.tanh(r)) ** 2)
         elif self.rbf_type == 'imq':
             self.rbf = lambda ep, r: 1. / np.sqrt(1 + (ep * r) ** 2)
         elif self.rbf_type == 'mat0':
-            self.rbf = lambda ep, r : np.exp(- ep * r)
+            self.rbf = lambda ep, r : np.exp(-ep * r)
         elif self.rbf_type == 'mat1':
             self.rbf = lambda ep, r: np.exp(-ep * r) * (1 + ep * r)
         elif self.rbf_type == 'mat2':
-            self.rbf = lambda ep, r: np.exp(- ep * r) * (3 + 3 * ep * r + (ep * r) ** 2)
+            self.rbf = lambda ep, r: np.exp(-ep * r) * (3 + 3 * ep * r + (ep * r) ** 2)
+        elif self.rbf_type ==  'wen':   
+            k = 0
+            d = 2
+            l = np.floor(d / 2) + k + 1
+            c = np.math.factorial(l + 2 * k) / np.math.factorial(l)
+            e = l + k
+#            if k == 0:
+#                p = '1';
+#            elif k == 1:
+#                p = '(l + 1) * r + 1'
+#            elif k == 2:
+#                p = '(l + 3) * (l + 1) * r .^ 2 + 3 * (l + 2) * r + 3'
+#            elif k == 3:
+#                p = '(l + 5) * (l + 3) * (l + 1) * r .^ 3 + (45 + 6 * l * (l + 6)) * r .^ 2 + (15 * (l + 3)) * r + 15'
+#            elif k == 4:
+#                p = '(l + 7) * (l + 5) * (l + 3) * (l + 1) * r .^ 4 + (5 * (l + 4) * (21 + 2 * l * (8 + l))) * r .^ 3 + (45 * (14 + l * (l + 8))) * r .^ 2 + (105 * (l + 4)) * r + 105'
+#            elif:
+#                raise Exception('This Wendland kernel is not implemented')
+            self.rbf = lambda ep, r: np.max(1 - ep * r, 0) ** e * ((l + 1) * ep * r + 1) / c
+           
         else:
             self.rbf = None
             raise Exception('This RBF is not implemented')
@@ -43,6 +63,9 @@ class RBF(Kernel):
     def diagonal(self, X):
         return np.ones((X.shape[0], 1)) * self.rbf(self.ep, 0)
     
+    def __str__(self):
+     return self.rbf_type + ' [gamma = %2.2e]' % self.ep   
+
     
 class polynomial(Kernel):
     def __init__(self, a=0, p=1):
@@ -54,6 +77,9 @@ class polynomial(Kernel):
     
     def diagonal(self, X):
         return ((np.linalg.norm(X, axis=1) + self.a) ** self.p)[:, None]
+
+    def __str__(self):
+     return 'polynomial' + ' [a = %2.2e, p = %2.2e]' % (self.a, self.p)   
 
 
 def main():
--- vkoga.py
+++ vkoga.py
@@ -8,29 +8,39 @@ Created on Sat Oct 26 14:44:27 2019
 # Guardare se con PyTorch va meglio
 #Check scikit-learn compatibility: https://scikit-learn.org/dev/developers/develop.html
 
-from kernels import Kernel
+from kernels import Kernel, RBF
 import numpy as np
 import matplotlib.pyplot as plt
-
+from sklearn.base import BaseEstimator
     
-class vkoga(object):
+class vkoga(BaseEstimator):
     ### TODO: Next steps  
-    ### TODO:  * parameter parsing (like scikit) 
-    ### TODO:  * reorth.
+    ### TODO:  * reorthogonalization
     ### TODO:  * decent comments & documentation
-    ### TODO:  * stampa messaggio (con str(kernel))
-    ### TODO:  * rimuovere Afun
     ### TODO:  * jupyter
     ### TODO:  * test scikit compatibility
-
+    ### TODO:  * guardare check_X_y, check_is_fitted(), ...
     
-    
-    def __init__(self, kernel):
+    def __init__(self, kernel=RBF(), verbose=True, 
+                 greedy_type='p_greedy', reg_par=0, restr_par=0, 
+                 tol_f=1e-10, tol_p=1e-10, max_iter=100):
+        
+        super(vkoga, self).__init__()
+        
+        # Set the verbosity on/off
+        self.verbose = verbose
+        
+        # Set the params defining the method 
         self.kernel = kernel
-        self.coef_ = None
-        self.ctrs_ = None
-        self.verbose = True
-     
+        self.greedy_type = greedy_type
+        self.reg_par = reg_par
+        self.restr_par = restr_par
+        
+        # Set the stopping values
+        self.max_iter = max_iter
+        self.tol_f = tol_f
+        self.tol_p = tol_p
+        
     def selection_rule(self, f, p):
         # Move to 2 norm (for vectorial outputs) 
         f = np.sum(f ** 2, axis=1)
@@ -53,50 +63,73 @@ class vkoga(object):
         A = self.kernel.eval(X, X)
         self.coef_ = np.linalg.solve(A, y)
 
-
     def fit(self, X, y):
-        y = np.array(y)
-        self.greedy_type = 'p_greedy'
-        reg_par = 1e-5
-        tol_F = 1e-10
-        tol_P = 1e-12
-        max_iter = 100
+       
+        # Initialize the model (cold start)
+        self.coef_ = None
+        self.ctrs_ = None
         
+        # Initialize the convergence train_hist (cold start)
+        self.train_hist = {}
+        self.train_hist['n'] = []
+        self.train_hist['f'] = []
+        self.train_hist['p'] = []
+        
+        # Initialize the residual
+        y = np.array(y)
+        
+        # Get the data dimension        
         N, q = y.shape         
-        max_iter = min(max_iter, N)
+
+        # Check the data dimension
+        if X.shape[0] != N:
+            raise ValueError('X.shape[0] should be the same as y.shape[0]')
+
+
+
+
+
+        self.max_iter = min(self.max_iter, N)#Check
+        
+        
         indI = []
         notIndI = list(range(N))
-        Vx = np.zeros((N, max_iter))
-        c = np.zeros((max_iter, q))
-        f_max = np.zeros((max_iter, 1))
-        p_max = np.zeros((max_iter, 1))
-        p = self.kernel.diagonal(X) + reg_par
-        Cut = np.zeros((max_iter, max_iter))       
+        Vx = np.zeros((N, self.max_iter))
+        c = np.zeros((self.max_iter, q))
+
+        f_max = np.zeros((self.max_iter, 1))
+        p_max = np.zeros((self.max_iter, 1))
+        p = self.kernel.diagonal(X) + self.reg_par
+        Cut = np.zeros((self.max_iter, self.max_iter))       
         
-        # Thus was useful in the Matlab version to avoid passing X, y to this 
-        # function. But here it is required by the scikit-learn interface
-        Afun = lambda i, j: ker.eval(X[i, :], X[j, :]) #+ reg_par * (i[:, None] == j[:, None].transpose())
-        
+        self.print_message('begin')
         # Iterative selection of new points
-        for n in range(max_iter):
+        for n in range(self.max_iter):
+            # prepare
+            self.train_hist['n'].append(n+1)
+            self.train_hist['f'].append([])
+            self.train_hist['p'].append([])
             # select the current index
-            idx, f_max[n], p_max[n] = self.selection_rule(y[notIndI], p[notIndI])
+            idx, self.train_hist['f'][n], self.train_hist['p'][n] = self.selection_rule(y[notIndI], p[notIndI])
             # add the current index
             indI.append(notIndI[idx])
             # check if the tolerances are reacheded
-            if f_max[n] <= tol_F:
+            if self.train_hist['f'][n] <= self.tol_f:
                 n = n - 1
-                self.print_message('f-tolerance reached: stop')   
+                self.print_message('end')   
                 break
-            if p_max[n] <= tol_P:
+            if self.train_hist['p'][n] <= self.tol_p:
                 n = n - 1
-                self.print_message('p-tolerance reached: stop')   
+                self.print_message('end')   
                 break
-            # If not, add the current point    
             # compute the nth basis
-            Vx[notIndI, n] = Afun(notIndI, indI[n])[:, 0] - Vx[notIndI, :n+1] @ Vx[indI[n], 0:n+1].transpose()
+            Vx[notIndI, n] = self.kernel.eval(X[notIndI, :], X[indI[n],:])[:, 0] - Vx[notIndI, :n+1] @ Vx[indI[n], 0:n+1].transpose()
+            Vx[indI[n], n] += self.reg_par
             # normalize the nth basis
             Vx[notIndI, n] = Vx[notIndI, n] / np.sqrt(p[indI[n]])
+#           reorthogonalization 
+#           if n > 2:
+#                breakpoint()
             # update the change of basis
             Cut_new_row = np.ones(n + 1)
             Cut_new_row[:n] = -Vx[indI[n], :n] @ Cut[:n:, :n]
@@ -110,7 +143,7 @@ class vkoga(object):
             # remove the nth index from the dictionary
             notIndI.pop(idx)
         else:
-            self.print_message('max_iter reached: stop')              
+            self.print_message('end')              
 
         # define coefficients and centers
         c = c[:n+1, :]
@@ -118,97 +151,60 @@ class vkoga(object):
         indI = indI[:n+1]
         self.coef_ = Cut.transpose() @ c
         self.ctrs_ = X[indI, :]
-#        fmax = np.sqrt(fmax(1 : n));
-#        pmax = np.sqrt(pmax(1 : n));
-        return self, f_max, p_max
+        f_max = np.sqrt(f_max[:n+1]);
+        p_max = np.sqrt(p_max[:n+1]);
+        return self, self.train_hist['f'], self.train_hist['p']
 
     def predict(self, x):
         return self.kernel.eval(x, self.ctrs_) @ self.coef_     
 
-    def print_message(self, message):
-        base_str = '[VKOGA] ' 
-        base_str += 'Iteration terminated after n = %2.d' % self.coef_.shape[0]  
-        if self.verbose:
-            print(base_str + message)
-
-#from kernels import RBF, polynomial
-#ker = RBF(rbf_type='gauss', ep=4)
-##ker = RBF(rbf_type='mat2', ep=4)
-##ker = polynomial(a=0, p=2)
-#
-#f = lambda x: np.array([np.cos(10 * x), np.sin(10 * x)])[:,:,0].transpose()
-#X = np.random.rand(1000, 2)
-#y = X
-#
-#model = vkoga(ker)
-#
-#_, f_max, p_max = model.fit(X, y)
-#
-#X_te = np.random.rand(10000, 2)
-#s_te = model.predict(X_te)
-#y_te = X_te
-#s = model.predict(X)
-#
-#
-##%%
-#fig = plt.figure(1)
-#fig.clf()
-#ax = fig.gca()
-#ax.plot(X[:, 0], X[:, 1], '.')
-#ax.plot(model.ctrs_[:, 0], model.ctrs_[:, 1], 'o')
-#ax.legend(['All points', 'Selected points'])
-#ax.grid()
-#fig.show()
-#
-#fig = plt.figure(2)
-#fig.clf()
-#ax = fig.gca()
-#ax.loglog(f_max)
-#ax.loglog(p_max)
-#ax.legend(['f_max', 'p_max'])
-#ax.grid()
-#fig.show()
+    def print_message(self, when):
+        
+        if self.verbose and when == 'begin':
+            print('*' * 30 + ' [VKOGA] ' + '*' * 30)
+            print('Training model with')
+            print('       |_ kernel              : %s' % self.kernel)
+            print('       |_ regularization par. : %2.2e' % self.reg_par)
+            print('       |_ restriction par.    : %2.2e' % self.restr_par)
+            print('')
+            
+        if self.verbose and when == 'end':
+            print('Training completed with')
+            print('       |_ selected points     : %8d / %8d' % (self.train_hist['n'][-1], self.max_iter))
+            print('       |_ train residual      : %2.2e / %2.2e' % (self.train_hist['f'][-1], self.tol_f))
+            print('       |_ train power fun     : %2.2e / %2.2e' % (self.train_hist['p'][-1], self.tol_p))
+            
 
 
 
-
-    
-from kernels import RBF, polynomial
+#%%
+from kernels import polynomial
 ker = RBF(rbf_type='gauss', ep=4)
-#ker = polynomial(a=0, p=5)
+#kernel = RBF(rbf_type='mat2', ep=4)
+#ker = RBF(rbf_type='wen', ep=4)
+#ker = polynomial(a=0, p=2)
 
 f = lambda x: np.array([np.cos(10 * x), np.sin(10 * x)])[:,:,0].transpose()
-X = np.linspace(-1, 1, 10000)[:, None]
-y = f(X)
+X = np.random.rand(10000, 2)
+y = X
 
-model = vkoga(ker)
+model = vkoga(kernel=ker)
 
 _, f_max, p_max = model.fit(X, y)
 
-X_te = np.linspace(-1, 1, 1000)[:, None]
+X_te = np.random.rand(10000, 2)
 s_te = model.predict(X_te)
-y_te = f(X_te)
+y_te = X_te
 s = model.predict(X)
 
-#y_predicted = vkoga(C=100).fit(X_train, y_train).predict(X_test)
 
 #%%
 fig = plt.figure(1)
 fig.clf()
-ax = fig.add_subplot(2, 1, 1)
-ax.plot(X, y[:, 0], 'o')
-ax.plot(X, s[:, 0], '.')
-ax.plot(X_te, y_te[:, 0], '-')
-ax.plot(X_te, s_te[:, 0], '-')
-ax.legend(['Train', 'Train prediction', 'Test', 'Test prediction'])
-ax.grid()
-
-ax = fig.add_subplot(2, 1, 2)
-ax.plot(X, y[:, 1], 'o')
-ax.plot(X, s[:, 1], '.')
-ax.plot(X_te, y_te[:, 1], '-')
-ax.plot(X_te, s_te[:, 1], '-')
-ax.legend(['Train', 'Train prediction', 'Test', 'Test prediction'])
+ax = fig.gca()
+#ax.plot(X[:, 0], X[:, 1], '.')
+ax.plot(model.ctrs_[:, 0], model.ctrs_[:, 1], '.')
+ax.legend(['All points', 'Selected points'])
 ax.grid()
 fig.show()
 
@@ -220,6 +216,74 @@ ax.loglog(p_max)
 ax.legend(['f_max', 'p_max'])
 ax.grid()
 fig.show()
+
+
+
+
+    
+#from kernels import RBF, polynomial
+#ker = RBF(rbf_type='gauss', ep=4)
+##ker = polynomial(a=1, p=5)
+#
+#f = lambda x: np.array([np.cos(10 * x), np.sin(10 * x)])[:,:,0].transpose()
+#X = np.linspace(-1, 1, 10000)[:, None]
+#y = f(X)
+#
+#model = vkoga(ker)
+#
+#_, f_max, p_max = model.fit(X, y)
+#
+#X_te = np.linspace(-1, 1, 1000)[:, None]
+#s_te = model.predict(X_te)
+#y_te = f(X_te)
+#s = model.predict(X)
+#
+##y_predicted = vkoga(C=100).fit(X_train, y_train).predict(X_test)
+#
+##%%
+#fig = plt.figure(1)
+#fig.clf()
+#ax = fig.add_subplot(2, 1, 1)
+#ax.plot(X, y[:, 0], 'o')
+#ax.plot(X, s[:, 0], '.')
+#ax.plot(X_te, y_te[:, 0], '-')
+#ax.plot(X_te, s_te[:, 0], '-')
+#ax.legend(['Train', 'Train prediction', 'Test', 'Test prediction'])
+#ax.grid()
+#
+#ax = fig.add_subplot(2, 1, 2)
+#ax.plot(X, y[:, 1], 'o')
+#ax.plot(X, s[:, 1], '.')
+#ax.plot(X_te, y_te[:, 1], '-')
+#ax.plot(X_te, s_te[:, 1], '-')
+#ax.legend(['Train', 'Train prediction', 'Test', 'Test prediction'])
+#ax.grid()
+##fig.show()
+#
+#fig = plt.figure(2)
+#fig.clf()
+#ax = fig.gca()
+#ax.semilogy(f_max)
+#ax.semilogy(p_max)
+#ax.legend(['f_max', 'p_max'])
+#ax.grid()
+##fig.show()
+
+
+#%%
+from sklearn.model_selection import GridSearchCV
+
+params = {'reg_par': np.logspace(-10, 1, 10)}
+
+#kernel=RBF(), verbose=True, greedy_type='p_greedy', reg_par=0, restr_par=0, tol_f=1e-10, tol_p=1e-10, max_iter=100)
+
+model = GridSearchCV(vkoga(), params, n_jobs=1, cv=5, iid='False', refit=True, scoring='max_error')  
+
+
+model.fit(X, y)
+
+
+
 
 
 
